28c61950bc4fcfe6093bb567170c3ecc27c4cb7e
nayan-ml <nayan.paul@silicon.ac.in>
2025-08-21 20:36:11 +0530

    Create Question-13.py

---
+++ Question-13.py
@@ -0,0 +1,3 @@
+'''Write a Python program to enter marks in 4 subjects of a student and find the percentage of marks, assuming that the full mark of each subject is 100.
+
+Your code starts from here'''
