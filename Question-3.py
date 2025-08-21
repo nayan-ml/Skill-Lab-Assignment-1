47259e89fd2388a9f7c8db31b33d8111ba76f8b3
nayan-ml <nayan.paul@silicon.ac.in>
2025-08-21 20:24:14 +0530

    Create Question-3.py

---
+++ Question-3.py
@@ -0,0 +1,11 @@
+# Write a Python program to print the following structure:
+'''
+   *****************************
+   * College: Silicon          *
+   * Address: Silicon Hills    *
+   * Near DLF Building         *
+   * Patia                     *
+   * Bhubaneswar               *
+   * 751024                    *
+   *****************************   
+   Your code starts from here'''
