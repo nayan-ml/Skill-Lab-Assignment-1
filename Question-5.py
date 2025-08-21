3918d9ac998b4bcaad3ea7ab2d56b3d7957fdc63
nayan-ml <nayan.paul@silicon.ac.in>
2025-08-21 20:27:14 +0530

    Create Question-5.py

---
+++ Question-5.py
@@ -0,0 +1,10 @@
+'''  
+Write a Python program to enter two numbers and find their sum, difference,
+multiplication, and division display the output in the following format:
+Enter two numbers
+70 10
+70 + 10 = 80
+70 – 10 = 60
+70 * 10 = 700
+70 / 10 = 7
+Your code starts from here'''
