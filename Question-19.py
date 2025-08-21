41e734c7a346c4db48976ed36c27b5e27aca8697
nayan-ml <nayan.paul@silicon.ac.in>
2025-08-21 20:43:42 +0530

    Create Question-19.py

---
+++ Question-19.py
@@ -0,0 +1,4 @@
+''' Write a Python program to calculate the gross salary of an employee by giving the basic
+salary as input, using the following formula.DA (60%) HRA(15%) Gross salary= Basic +DA + HRA.
+
+Your code starts from here'''
