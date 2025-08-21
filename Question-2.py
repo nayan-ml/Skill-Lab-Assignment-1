596957db721a8c95a2babb782ab0d3111c16d5f1
nayan-ml <nayan.paul@silicon.ac.in>
2025-08-21 20:20:24 +0530

    Create Question-2.py

---
+++ Question-2.py
@@ -0,0 +1,4 @@
+# Write a Python program by using a variable to represent a person’s name, and print
+# a message to that person. Your message should be simple, such as, “Hello Hari,
+# would you like to learn some Python today?”
+''' your code starts from here'''
